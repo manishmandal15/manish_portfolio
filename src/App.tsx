a300d552a8819d862636629633953cc7cf2e12d4
manishmandal15 <141217087+manishmandal15@users.noreply.github.com>
2026-07-24 00:31:40 +0530

    footer updatedd

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Skills from "./components/Skills/Skills";
 import Experience from "./components/Experience/Experience";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/footer"
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
